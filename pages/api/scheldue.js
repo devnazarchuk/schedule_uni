72415ab36359eb18a8b3c3ee12d12ce1825de979
artemnchuk <artemnchuk@gmail.com>
2024-09-28 20:22:06 +0200

    replace

--- pages/api/scheldue.js
+++ pages/api/scheldue.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   const { start, end } = req.query;
 
+  if (!start || !end) {
+    return res.status(400).json({ error: "Missing start or end date" });
+  }
+
   const apiUrl = `https://vnz.osvita.net/WidgetSchedule.asmx/GetScheduleDataX?callback=jsonp1727542856195&aVuzID=11613&aStudyGroupID=%22K2K3ZY5AE2ZA%22&aStartDate=%22${start}%22&aEndDate=%22${end}%22&aStudyTypeID=null`;
 
   try {
